3fe7f01aeb7c178d935e311e314d9eec377c1546
Rafael Almeida <almeidarp.dev@gmail.com>
2024-09-29 09:20:08 -0300

    feat: implements access when logging in

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,88 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { User } from '@prisma/client';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthRegisterDTO } from './dto/auth-register.dto';
+import { UserService } from 'src/user/user.service';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private readonly jtwervice: JwtService,
+    private readonly prisma: PrismaService,
+    private readonly userService: UserService,
+  ) {}
+
+  async createToken(user: User) {
+    return {
+      accessToken: this.jtwervice.sign(
+        {
+          id: user.id,
+          name: user.name,
+          email: user.email,
+        },
+        {
+          expiresIn: '7 days',
+          subject: String(user.id),
+          issuer: 'login',
+          audience: 'users',
+        },
+      )
+    }
+  }
+
+  async checkToken(token: string) {
+    // return this.jtwervice.verify()
+  }
+
+  async login(email: string, password: string) {
+    const user = await this.prisma.user.findFirst({
+      where: {
+        email,
+        password,
+      },
+    });
+
+    if (!user) {
+      throw new UnauthorizedException('Email e/ou senha incorretos');
+    }
+
+    return this.createToken(user);
+  }
+
+  async forget(email: string) {
+    const user = await this.prisma.user.findFirst({
+      where: {
+        email,
+      },
+    });
+
+    if (!user) {
+      throw new UnauthorizedException('Email está incorreto');
+    }
+    // TO DO: Enviar o e-mail ao usuário
+    return true;
+  }
+
+  async reset(password: string, token: string) {
+    // TO DO: Validar o token...
+
+    const id = 0;
+
+    const user = await this.prisma.user.update({
+      where: {
+        id,
+      },
+      data: {
+        password,
+      },
+    });
+    return this.createToken(user);
+  }
+
+  async register(data: AuthRegisterDTO) {
+    const user = await this.userService.create(data);
+
+    return this.createToken(user);
+  }
+}
