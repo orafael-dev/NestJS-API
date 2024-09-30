6f649fb78ceebcda4f7374ebf4a9a4e1f7e1535e
Rafael Almeida <almeidarp.dev@gmail.com>
2024-09-29 21:21:32 -0300

    feat: enable Cors

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { LogInterceptor } from './user/interceptors/LogInterceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors()
+
   app.useGlobalPipes(new ValidationPipe())
-  // app.useGlobalInterceptors(new LogInterceptor())
 
   await app.listen(3000);
 }
